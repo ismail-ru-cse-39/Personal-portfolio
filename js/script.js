e62e1d824bddead54250a9ab256b2f0d03eba17b
ismail-ru-cse-39 <shaikat.ru.39@gmail.com>
2020-06-16 16:47:32 +0000

    using java script

---
+++ js/script.js
@@ -0,0 +1,31 @@
+//Portfolio item filter
+
+const filterContainer = document.querySelector(".portfolio-filter"),
+	filterBtns = filterContainer.children,
+	totalFilterBtn = filterBtns.length,
+	portfolioItems = document.querySelectorAll(".portfolio-item"),
+	totalPortfolioItems = portfolioItems.length;
+
+
+	for(let i = 0 ; i  < totalFilterBtn; i++) {
+		filterBtns[i].addEventListener("click", function() {
+			filterContainer.querySelector(".active").classList.remove("active");
+			this.classList.add("active");
+
+			const filterValue = this.getAttribute("data-filter");
+			for(let k=0; k < totalPortfolioItems; k++) {
+				if(filterValue === portfolioItems[k].getAttribute("data-category")) {
+					portfolioItems[k].classList.remove("hide");
+					portfolioItems[k].classList.add("show");
+				} else{
+					portfolioItems[k].classList.remove("show");
+					portfolioItems[k].classList.add("hide");
+				}
+				if(filterValue == "all") {
+					portfolioItems[k].classList.remove("hide");
+					portfolioItems[k].classList.add("show");
+				}
+			}
+		})
+	}
+
