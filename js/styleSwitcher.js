4376edf156981f06d843deaecb6f292dabd03f79
ismail-ru-cse-39 <shaikat.ru.39@gmail.com>
2020-07-14 19:42:56 +0000

    Style switcher is completed.

--- js/styleSwitcher.js
+++ js/styleSwitcher.js
@@ -4,13 +4,27 @@ const links = document.querySelectorAll(".alternate-style");
 
 function setActiveStyle(color) {
 	for(let i = 0; i < totalLenghth; i++) {
-		if(color == links[i].getAttribute("title")) {
+		if(color === links[i].getAttribute("title")) {
 			links[i].removeAttribute("disabled");
 		} else {
 			links[i].setAttribute("disabled", "true");
 		}
 	}
 }
+
+//body skin
+
+const bodySkins = document.querySelectorAll(".body-skin"),
+	  totalBodySkin = bodySkins.length;
+	  for(let i = 0 ; i < totalBodySkin; i++) {
+	  	bodySkins[i].addEventListener("change", function(){
+	  		if(this.value === "dark") {
+	  			document.body.className = "dark";
+	  		} else {
+	  			document.body.className="light";
+	  		}
+	  	})
+	  }
 
 document.querySelector(".toggle-style-switcher").addEventListener("click",() =>{
 	console.log("hi");
